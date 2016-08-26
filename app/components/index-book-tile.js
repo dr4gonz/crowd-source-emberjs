49c08526080050c992a108dd7dd592906f5c26e1
Matthew <mreyez@gmail.com>
2016-08-26 13:53:29 -0700

    add computed property for title and name

--- app/components/index-book-tile.js
+++ app/components/index-book-tile.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  bookTitleAuthor: Ember.computed('book.title', 'book.author', function(){
+    return this.get('book.title') + ' By: ' + this.get('book.author');
+  })
 });
