735939adb27175072c139f9b3a78a5633e078ea0
Matthew <mreyez@gmail.com>
2016-08-26 22:02:42 -0700

    add styling

--- app/components/index-book-tile.js
+++ app/components/index-book-tile.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   bookTitleAuthor: Ember.computed('book.title', 'book.author', function(){
-    return this.get('book.title') + ' By: ' + this.get('book.author');
+    return this.get('book.title') + ' By ' + this.get('book.author');
   })
 });
